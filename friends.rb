24cd3c6a301369c845a5e1d951431654aeffcd99
Samuel Bacon <sbacon803@gmail.com>
2022-04-21 18:24:01 -0600

    Add under 60

--- friends.rb
+++ friends.rb
@@ -1,7 +1,18 @@
 integers = [4, 20, 24, 28, 35, 70, 80, 90]
 
-integers.each do |integer|
-  if integer > 30
-    puts integer
+# integers.each do |integer|
+#   if integer > 30
+#     puts integer
+#   end
+# end
+
+def under_60(a)
+  a.each do |integer|
+    if integer < 60
+      puts integer
+    end
   end
 end
+
+a = integers
+under_60(a)
