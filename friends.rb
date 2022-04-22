297f934842af2cb5d4b17c61dce8cfd328279283
Samuel Bacon <sbacon803@gmail.com>
2022-04-21 18:29:09 -0600

    Add first code back in

--- friends.rb
+++ friends.rb
@@ -1,10 +1,10 @@
 integers = [4, 20, 24, 28, 35, 70, 80, 90]
 
-# integers.each do |integer|
-#   if integer > 30
-#     puts integer
-#   end
-# end
+integers.each do |integer|
+  if integer > 30
+    puts integer
+  end
+end
 
 def under_60(a)
   a.each do |integer|
